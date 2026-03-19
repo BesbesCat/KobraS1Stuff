06a464670bab5c7485eb20a50e3046ec2b0b61ff
BesbesCat <162553546+BesbesCat@users.noreply.github.com>
2026-03-19 19:57:02 +0200

    Update installer.js

--- installer.js
+++ installer.js
@@ -2,7 +2,7 @@ const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 async function runInstaller() {
     const status = document.getElementById('install-progress');
     const TAR_LIB_URL = "https://cdn.jsdelivr.net/npm/js-untar@latest/build/dist/untar.js";
-    const BUNDLE_URL = "https://cdn.jsdelivr.net/gh/BesbesCat/KobraS1Stuff@main/update.tar";
+    const BUNDLE_URL = "https://cdn.jsdelivr.net/gh/BesbesCat/KobraS1Stuff@master/update.tar";
 
     try {
         // 1. Load the untar library if it's not already there
